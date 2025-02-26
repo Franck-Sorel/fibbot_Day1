99a9bec1a5bf0d58ebb2cbc713e1316bf25fc6ce
Franck-Sorel <mfrancksorel@gmail.com>
2025-02-26 16:14:04 +0100

    Update main.rs

--- fibbot/src/main.rs
+++ fibbot/src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Usage: {} <input1> <input2>", args[0]);
+        std::process::exit(1);
+    }
+
+    let input1 = &args[1];
+    let input2 = &args[2];
+
+    println!("Input 1: {}", input1);
+    println!("Input 2: {}", input2);
 }
